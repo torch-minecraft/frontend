fae080488c22e2dec257dfb6cfe2dbed4da24add
omegaladon <businessomga@gmail.com>
2023-05-20 14:52:20 -0700

    Finish 404 page

--- src/components/notfound.js
+++ src/components/notfound.js
@@ -1,7 +1,33 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import Title from "./title";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 page not found</h1>
-    </div>
+    <Container maxWidth="xl">
+      <Title />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        margin={15}
+      >
+        <Typography variant="h2" fontFamily="Poppins" fontSize={35}>
+          Oops! Looks like you got lost!
+        </Typography>
+        <Button
+          sx={{
+            textTransform: "none",
+            margin: 3,
+          }}
+          variant="outlined"
+          size="large"
+        >
+          <Typography variant="h2" fontFamily="Poppins" fontSize={20} padding={2}>
+            Let's get you back home
+          </Typography>
+        </Button>
+      </Box>
+    </Container>
   );
 }
